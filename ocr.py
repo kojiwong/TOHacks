8c843724d0f3131e68c4c06d28da99f64c9e765e
Mikipswso <mickey4649soccer@gmail.com>
2022-05-29 02:13:50 -0400

    done OCR

--- ocr.py
+++ ocr.py
@@ -1,10 +1,23 @@
 from pdf2image import convert_from_path
+from PIL import Image
+from pytesseract import pytesseract
 
-pdfs = r"/Users/kojimacbook/PycharmProjects/TOHacks/Koji Wong Resume.pdf"
-pages = convert_from_path(pdfs)
+pdfs = r"C:\Users\Paula\TOHacks\Koji Wong Resume.pdf"
+pages = convert_from_path(pdfs, 500)
 
 i = 1
 for page in pages:
-    image_name = "Page" + str(i) + ".jpg"
+    image_name = "Page_" + str(i) + ".jpg"
     page.save(image_name, "JPEG")
-    i = i+1
+    i += 1
+
+image_path = r"C:\Users\Paula\TOHacks\Page_1.jpg"
+img = Image.open(image_path)
+pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+text = pytesseract.image_to_string(img)
+text_list = text.split('\n')
+text_list = [i for i in text_list if i != '']
+clean_text = ''.join(text_list)
+
+print(clean_text)
